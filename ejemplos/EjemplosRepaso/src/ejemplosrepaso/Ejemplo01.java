b62043b027be4d97bed5f1be85c2d3144e529995
Xavier Chavez <xavierchavez916@gmail.com>
2020-12-02 09:44:55 -0500

    resolucion

--- ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo01.java
+++ ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo01.java
@@ -17,14 +17,18 @@ public class Ejemplo01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner entrada = new Scanner(System.in);
-        String ciudad = "Loja";
-        String pais  = "Ecuador";
-        
-        System.out.printf("%s,ciudad\ndel país:\n    %s", ciudad, pais);
         // 
+        Scanner entrada = new Scanner(System.in);
+        String ciudad ;
+        String pais ;
         
+        System.out.println("Ingrese la ciudad");
+        ciudad = entrada.nextLine();
+        
+        System.out.println("Ingrese el país");
+        pais= entrada.nextLine();
+        
+        System.out.printf("%s, ciudad\ndel país:\n\t%s.", ciudad, pais);
     }
     
 }
